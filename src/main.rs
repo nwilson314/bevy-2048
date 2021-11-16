4dff458b8568a5bbf4ba266b00c954acace50a14
Nate Wilson <nsw.wilson@gmail.com>
2021-11-16 08:31:11 -0800

    First commit

---
+++ src/main.rs
@@ -0,0 +1,540 @@
+use bevy::{prelude::*};
+use bevy_easings::*;
+use itertools::Itertools;
+use rand::prelude::*;
+use std::{cmp::Ordering, collections::HashMap, convert::{TryFrom, TryInto}, ops::Range};
+
+mod ui;
+use ui::*;
+
+const TILE_SIZE: f32 = 40.0;
+const TILE_SPACER: f32 = 10.0;
+
+struct Board {
+    size: u8,
+    physical_size: f32,
+}
+
+impl Board {
+    fn new(size: u8) -> Self {
+        let physical_size = f32::from(size) * TILE_SIZE
+            + f32::from(size + 1) * TILE_SPACER;
+        Board {
+            size,
+            physical_size,
+        }
+    }
+    fn cell_position_to_physical(&self, pos: u8) -> f32 {
+        let offset =
+            -self.physical_size / 2.0 + 0.5 * TILE_SIZE;
+
+        offset
+            + f32::from(pos) * TILE_SIZE
+            + f32::from(pos + 1) * TILE_SPACER
+    }
+}
+
+struct Materials {
+    board: Handle<ColorMaterial>,
+    tile_placeholder: Handle<ColorMaterial>,
+    tile: Handle<ColorMaterial>,
+    none: Handle<ColorMaterial>,
+}
+
+impl FromWorld for Materials {
+    fn from_world(world: &mut World) -> Self {
+        let mut materials = world
+            .get_resource_mut::<Assets<ColorMaterial>>()
+            .unwrap();
+        Materials {
+            board: materials
+                .add(Color::rgb(0.7, 0.7, 0.8).into()),
+            tile_placeholder: materials
+                .add(Color::rgb(0.75, 0.75, 0.9).into()),
+            tile: materials
+                .add(Color::rgb(0.9, 0.9, 1.0).into()),
+            none: materials.add(Color::NONE.into()),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Points {
+    value: u32,
+}
+
+#[derive(Debug, PartialEq, Clone, Copy, Eq, Hash)]
+struct Position {
+    x: u8,
+    y: u8,
+}
+
+struct TileText;
+
+struct FontSpec {
+    family: Handle<Font>,
+}
+
+impl FromWorld for FontSpec {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world
+            .get_resource_mut::<AssetServer>()
+            .unwrap();
+        FontSpec {
+            family: asset_server
+                .load("fonts/FiraSans-Bold.ttf"),
+        }
+    }
+}
+
+enum BoardShift {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
+impl TryFrom<&KeyCode> for BoardShift {
+    type Error = &'static str;
+
+    fn try_from(value: &KeyCode) -> Result<Self, Self::Error> {
+        match value {
+            KeyCode::Left => Ok(BoardShift::Left),
+            KeyCode::Up => Ok(BoardShift::Up),
+            KeyCode::Right => Ok(BoardShift::Right),
+            KeyCode::Down => Ok(BoardShift::Down),
+            _ => Err("not a valid board_shift key"),
+        }
+    }
+}
+
+impl BoardShift {
+    fn sort(&self, a: &Position, b: &Position) -> Ordering {
+        match self {
+            BoardShift::Left => {
+                match Ord::cmp(&a.y, &b.y) {
+                    Ordering::Equal => Ord::cmp(&a.x, &b.x),
+                    ordering => ordering,
+                }
+            },
+            BoardShift::Right => {
+                match Ord::cmp(&b.y, &a.y) {
+                    Ordering::Equal => Ord::cmp(&b.x, &a.x),
+                    ordering => ordering,
+                }
+            },
+            BoardShift::Up => {
+                match Ord::cmp(&b.x, &a.x) {
+                    Ordering::Equal => Ord::cmp(&b.y, &a.y),
+                    ordering => ordering,
+                }
+            },
+            BoardShift::Down => {
+                match Ord::cmp(&a.x, &b.x) {
+                    Ordering::Equal => Ord::cmp(&a.y, &b.y),
+                    ordering => ordering,
+                }
+            }
+        }   
+    }
+    fn set_column_position(
+        &self,
+        board_size: u8,
+        position: &mut Mut<Position>,
+        index: u8,
+    ) {
+        match self {
+            BoardShift::Left => position.x = index,
+            BoardShift::Right => position.x = board_size - 1 - index,
+            BoardShift::Up => position.y = board_size - 1 - index,
+            BoardShift::Down => position.y = index,
+        }
+    }
+    fn get_row_position( &self, position: &Position) -> u8 {
+        match self {
+            BoardShift::Left | BoardShift::Right => position.y,
+            BoardShift::Up | BoardShift::Down => position.x,
+        }
+    }
+}
+
+struct NewTileEvent;
+
+#[derive(Default)]
+struct Game {
+    score: u32,
+    score_best: u32
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum RunState {
+    Playing,
+    GameOver,
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(GameUiPlugin)
+        .add_plugin(EasingsPlugin)
+        .init_resource::<Materials>()
+        .init_resource::<FontSpec>()
+        .init_resource::<Game>()
+        .add_event::<NewTileEvent>()
+        .add_state(RunState::Playing)
+        .add_startup_system(setup.system())
+        .add_startup_system(spawn_board.system())
+        .add_system_set(
+            SystemSet::on_update(RunState::Playing)
+                .with_system(render_tile_points.system())
+                .with_system(board_shift.system())
+                .with_system(render_tiles.system())
+                .with_system(new_tile_handler.system())
+                .with_system(end_game.system())
+        )
+        .add_system_set(
+            SystemSet::on_enter(RunState::Playing)
+                .with_system(game_reset.system())
+                .with_system(spawn_tiles.system())
+        )
+        .run()
+}
+
+fn setup(mut commands: Commands) {
+    commands
+        .spawn_bundle(OrthographicCameraBundle::new_2d());
+}
+
+fn spawn_board(
+    mut commands: Commands,
+    materials: Res<Materials>,
+) {
+    let board = Board::new(4);
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.board.clone(),
+            sprite: Sprite::new(Vec2::new(
+                board.physical_size,
+                board.physical_size,
+            )),
+            ..Default::default()
+        })
+        .with_children(|builder| {
+
+            for tile in (0..board.size)
+                .cartesian_product(0..board.size)
+            {
+                builder.spawn_bundle(SpriteBundle {
+                    material: materials
+                        .tile_placeholder
+                        .clone(),
+                    sprite: Sprite::new(Vec2::new(
+                        TILE_SIZE, TILE_SIZE,
+                    )),
+                    transform: Transform::from_xyz(
+                        board.cell_position_to_physical(
+                            tile.0
+                        ),
+                        board.cell_position_to_physical(
+                            tile.1,
+                        ),
+                        1.0,
+                    ),
+                    ..Default::default()
+                });
+            }
+        })
+        .insert(board);
+}
+
+fn spawn_tiles(
+    mut commands: Commands,
+    materials: Res<Materials>,
+    query_board: Query<&Board>,
+    font_spec: Res<FontSpec>,
+) {
+    let board = query_board
+        .single()
+        .expect("always expect a board");
+
+    let mut rng = rand::thread_rng();
+    let starting_tiles: Vec<(u8, u8)> = (0..board.size)
+        .cartesian_product(0..board.size)
+        .choose_multiple(&mut rng, 2);
+    for (x, y) in starting_tiles.iter() {
+        let pos = Position { x: *x, y: *y };
+        spawn_tile(
+            &mut commands,
+            &materials,
+            board,
+            &font_spec,
+            pos,
+        );
+    }
+}
+
+fn render_tile_points(
+    mut texts: Query<&mut Text, With<TileText>>,
+    tiles: Query<(&Points, &Children)>,
+) {
+    for (points, children) in tiles.iter() {
+        if let Some(entity) = children.first() {
+            let mut text = texts
+                .get_mut(*entity)
+                .expect("expected Text to exist");
+            let mut text_selection = text.sections.first_mut()
+                .expect("expect first section to be accessible as mutable");
+            text_selection.value = points.value.to_string();
+            if text_selection.value.parse::<i32>().unwrap() > 100 {
+                text_selection.style.font_size = 30.0;
+            }
+        }
+    }
+}
+
+fn board_shift(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut tiles: Query<(Entity, &mut Position, &mut Points)>,
+    query_board: Query<&Board>,
+    mut tile_writer: EventWriter<NewTileEvent>,
+    mut game: ResMut<Game>,
+) {
+    let board = query_board
+        .single()
+        .expect("expect there to be a board");
+    let shift_direction =
+        keyboard_input.get_just_pressed().find_map(
+            |key_code| BoardShift::try_from(key_code).ok(),
+        );
+    if let Some(board_shift) = shift_direction {
+        let mut it = tiles
+            .iter_mut().sorted_by(|a, b| board_shift.sort(&a.1, &b.1))
+            .peekable();
+        
+        let mut column: u8 = 0;
+
+        while let Some(mut tile) = it.next() {
+            board_shift.set_column_position(board.size, &mut tile.1, column);
+            if let Some(tile_next) = it.peek() {
+                if board_shift.get_row_position(&tile.1) != board_shift.get_row_position(&tile_next.1) {
+                    // different rows, don't merge
+                    column = 0;
+                } else if tile.2.value
+                    != tile_next.2.value
+                {
+                    // different values, don't merge
+                    column = column + 1;
+                } else {
+                    // merge
+                    let real_next_tile = it
+                            .next()
+                            .expect("A peeked tile should always exist when we .next here");
+                    tile.2.value = tile.2.value
+                        + real_next_tile.2.value;
+                    game.score += tile.2.value;
+
+                    commands
+                        .entity(real_next_tile.0)
+                        .despawn_recursive();
+    
+                    if let Some(future) = it.peek()
+                    {
+                        if board_shift.get_row_position(&tile.1) != board_shift.get_row_position(&future.1) {
+                            column = 0;
+                        } else {
+                            column = column + 1;
+                        }
+                    }
+                }
+            }  
+        }
+        tile_writer.send(NewTileEvent);
+    }
+    if game.score_best < game.score {
+        game.score_best = game.score;
+    }
+}
+
+fn render_tiles(
+    mut commands: Commands,
+    tiles: Query<
+        (Entity, &Transform, &Position),
+        Changed<Position>,
+    >,
+    query_board: Query<&Board>,
+) {
+    let board = query_board
+        .single()
+        .expect("expect there to be a board");
+        for (entity, transform, pos) in tiles.iter() {
+            let x = board.cell_position_to_physical(pos.x);
+            let y = board.cell_position_to_physical(pos.y);
+        
+            commands.entity(entity).insert(transform.ease_to(
+                Transform::from_xyz(
+                    x,
+                    y,
+                    transform.translation.z,
+                ),
+                EaseFunction::QuadraticInOut,
+                EasingType::Once {
+                    duration: std::time::Duration::from_millis(
+                        100,
+                    ),
+                },
+            ));
+        }
+}
+
+fn new_tile_handler(
+    mut tile_reader: EventReader<NewTileEvent>,
+    mut commands: Commands,
+    query_board: Query<&Board>,
+    materials: Res<Materials>,
+    tiles: Query<&Position>,
+    font_spec: Res<FontSpec>,
+) {
+    let board = query_board
+        .single()
+        .expect("expect there to always be a board");
+
+    for _event in tile_reader.iter() {
+        // insert new tile
+        let mut rng = rand::thread_rng();
+        let possible_position: Option<Position> = (0
+            ..board.size)
+            .cartesian_product(0..board.size)
+            .filter_map(|tile_pos| {
+                let new_pos = Position {
+                    x: tile_pos.0,
+                    y: tile_pos.1,
+                };
+                match tiles
+                    .iter()
+                    .find(|&&pos| pos == new_pos)
+                {
+                    Some(_) => None,
+                    None => Some(new_pos),
+                }
+            })
+            .choose(&mut rng);
+
+        if let Some(pos) = possible_position {
+            spawn_tile(
+                &mut commands,
+                &materials,
+                board,
+                &font_spec,
+                pos,
+            );
+        }
+    }
+}
+
+fn spawn_tile(
+    commands: &mut Commands,
+    materials: &Res<Materials>,
+    board: &Board,
+    font_spec: &Res<FontSpec>,
+    pos: Position,
+) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.tile.clone(),
+            sprite: Sprite::new(Vec2::new(
+                TILE_SIZE, TILE_SIZE,
+            )),
+            transform: Transform::from_xyz(
+                board.cell_position_to_physical(pos.x),
+                board.cell_position_to_physical(pos.y),
+                1.0,
+            ),
+            ..Default::default()
+        })
+        .with_children(|child_builder| {
+            child_builder
+                .spawn_bundle(Text2dBundle {
+                    text: Text::with_section(
+                        "2",
+                        TextStyle {
+                            font: font_spec.family.clone(),
+                            font_size: 40.0,
+                            color: Color::BLACK,
+                            ..Default::default()
+                        },
+                        TextAlignment {
+                            vertical: VerticalAlign::Center,
+                            horizontal:
+                                HorizontalAlign::Center,
+                        },
+                    ),
+                    transform: Transform::from_xyz(
+                        0.0, 0.0, 1.0,
+                    ),
+                    ..Default::default()
+                })
+                .insert(TileText);
+        })
+        .insert(Points { value: 2 })
+        .insert(pos);
+}
+
+fn end_game(
+    tiles: Query<(&Position, &Points)>,
+    query_board: Query<&Board>,
+    mut run_state: ResMut<State<RunState>>,
+) {
+    let board = query_board
+        .single()
+        .expect("expect there to be a board");
+
+    if tiles.iter().len() == 16 {
+        let map: HashMap<&Position, &Points> = 
+            tiles.iter().collect();
+
+        let neighbor_points = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+
+        let board_range: Range<i8> = 0..(board.size as i8);
+
+        let has_move = tiles.iter().any(
+            |(Position { x, y }, value)| {
+                neighbor_points
+                    .iter()
+                    .filter_map(|(x2, y2)| {
+                        let new_x = *x as i8 - x2;
+                        let new_y = *y as i8 - y2;
+                
+                        if !board_range.contains(&new_x)
+                            || !board_range.contains(&new_y)
+                        {
+                            return None;
+                        };
+                
+                        map.get(&Position {
+                            x: new_x.try_into().unwrap(),
+                            y: new_y.try_into().unwrap(),
+                        })
+                    })
+                    .any(|&v| v == value)
+        });
+
+        if has_move == false {
+            dbg!("game over!");
+            run_state.set(RunState::GameOver).unwrap();
+        }
+    }
+}
+
+fn game_reset(
+    mut commands: Commands,
+    tiles: Query<Entity, With<Position>>,
+    mut game: ResMut<Game>,
+) {
+    for entity in tiles.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+
+    game.score = 0;
+}  
---
+++ src/ui.rs
@@ -0,0 +1,287 @@
+use bevy::prelude::*;
+
+use crate::{FontSpec, Game, Materials, RunState};
+
+
+pub struct ScoreDisplay;
+
+pub struct BestScoreDisplay;
+
+struct ButtonMaterials {
+    normal: Handle<ColorMaterial>,
+    hovered: Handle<ColorMaterial>,
+    pressed: Handle<ColorMaterial>,
+}
+
+impl FromWorld for ButtonMaterials {
+    fn from_world(world: &mut World) -> Self {
+        let mut materials = world
+            .get_resource_mut::<Assets<ColorMaterial>>()
+            .unwrap();
+        ButtonMaterials {
+            normal: materials
+                .add(Color::rgb(0.75, 0.75, 0.9).into()),
+            hovered: materials
+                .add(Color::rgb(0.7, 0.7, 0.9).into()),
+            pressed: materials
+                .add(Color::rgb(0.6, 0.6, 1.0).into()),
+        }
+    }
+}
+
+pub struct GameUiPlugin;
+
+impl Plugin for GameUiPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.init_resource::<ButtonMaterials>()
+            .add_startup_system(setup_ui.system())
+            .add_system(scoreboard.system())
+            .add_system(button_interaction_system.system())
+            .add_system(button_text_system.system());
+    }
+}
+
+fn setup_ui(
+    mut commands: Commands,
+    materials: Res<Materials>,
+    font_spec: Res<FontSpec>,
+) {
+    commands.spawn_bundle(UiCameraBundle::default());
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                align_items: AlignItems::FlexEnd,
+                padding: Rect::all(Val::Px(50.0)),
+                ..Default::default()
+            },
+            material: materials.none.clone(),
+            ..Default::default()
+        })
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "2048",
+                    TextStyle {
+                        font: font_spec.family.clone(),
+                        font_size: 40.0,
+                        color: Color::WHITE,
+                    },
+                    TextAlignment::default(),
+                ),
+                ..Default::default()
+            });
+
+            parent
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        justify_content: JustifyContent::Center,
+                        size: Size::new(Val::Percent(100.0), Val::Auto),
+                        ..Default::default()
+                    },
+                    material: materials.none.clone(),
+                    ..Default::default()
+                })
+                .with_children(|parent| {
+                    // scorebox
+                    parent
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                flex_direction: FlexDirection::ColumnReverse,
+                                align_items: AlignItems::Center,
+                                margin: Rect {
+                                    left: Val::Px(20.0),
+                                    right: Val::Px(20.0),
+                                    top: Val::Px(0.0),
+                                    bottom: Val::Px(0.0),
+                                },
+                                padding: Rect::all(Val::Px(10.0)),
+                                ..Default::default()
+                            },
+                            material: materials.tile_placeholder.clone(),
+                            ..Default::default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn_bundle(TextBundle {
+                                text: Text::with_section(
+                                    "Score",
+                                    TextStyle {
+                                        font: font_spec.family.clone(),
+                                        font_size: 15.0,
+                                        color: Color::WHITE,
+                                    },
+                                    TextAlignment {
+                                        vertical: VerticalAlign::Center,
+                                        horizontal: HorizontalAlign::Center,
+                                    },
+                                ),
+                                ..Default::default()
+                            });
+                            parent
+                                .spawn_bundle(TextBundle {
+                                    text: Text::with_section(
+                                        "<score>",
+                                        TextStyle {
+                                            font: font_spec.family.clone(),
+                                            font_size: 20.0,
+                                            color: Color::WHITE,
+                                        },
+                                        TextAlignment {
+                                            vertical: VerticalAlign::Center,
+                                            horizontal: HorizontalAlign::Center,
+                                        },
+                                    ),
+                                    ..Default::default()
+                                })
+                                .insert(ScoreDisplay);
+                        });
+                    // end scorebox
+                    // best scorebox
+                    parent
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                flex_direction: FlexDirection::ColumnReverse,
+                                align_items: AlignItems::Center,
+                                padding: Rect::all(Val::Px(10.0)),
+                                ..Default::default()
+                            },
+                            material: materials.tile_placeholder.clone(),
+                            ..Default::default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn_bundle(TextBundle {
+                                text: Text::with_section(
+                                    "Best",
+                                    TextStyle {
+                                        font: font_spec.family.clone(),
+                                        font_size: 15.0,
+                                        color: Color::WHITE,
+                                    },
+                                    TextAlignment {
+                                        vertical: VerticalAlign::Center,
+                                        horizontal: HorizontalAlign::Center,
+                                    },
+                                ),
+                                ..Default::default()
+                            });
+                            parent
+                                .spawn_bundle(TextBundle {
+                                    text: Text::with_section(
+                                        "<score>",
+                                        TextStyle {
+                                            font: font_spec.family.clone(),
+                                            font_size: 20.0,
+                                            color: Color::WHITE,
+                                        },
+                                        TextAlignment {
+                                            vertical: VerticalAlign::Center,
+                                            horizontal: HorizontalAlign::Center,
+                                        },
+                                    ),
+                                    ..Default::default()
+                                })
+                                .insert(BestScoreDisplay);
+                        });
+                    // end best scorebox
+                });
+            parent
+                .spawn_bundle(ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(100.0), Val::Px(30.0)),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                .with_children(|parent| {
+                    parent.spawn_bundle(TextBundle {
+                        text: Text::with_section(
+                            "Button",
+                            TextStyle {
+                                font: font_spec.family.clone(),
+                                font_size: 20.0,
+                                color: Color::rgb(0.9, 0.9, 0.9),
+                            },
+                            Default::default(),
+                        ),
+                        ..Default::default()
+                    });
+                });
+        });
+}
+
+fn scoreboard (
+    game: Res<Game>,
+    mut query_scores: QuerySet<(
+        Query<&mut Text, With<ScoreDisplay>>,
+        Query<&mut Text, With<BestScoreDisplay>>
+    )>,
+) {
+    let mut text = query_scores.q0_mut().single_mut().unwrap();
+    text.sections[0].value = game.score.to_string();
+
+    let mut text = query_scores.q1_mut().single_mut().unwrap();
+    text.sections[0].value = game.score_best.to_string();
+}
+
+fn button_interaction_system(
+    button_materials: Res<ButtonMaterials>,
+    mut interaction_query: Query<
+        (&Interaction, &mut Handle<ColorMaterial>),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut run_state: ResMut<State<RunState>>,
+) {
+    for (interaction, mut material) in
+        interaction_query.iter_mut()
+    {
+        match interaction {
+            Interaction::Clicked => {
+                *material = button_materials.pressed.clone();
+                match run_state.current() {
+                    RunState::Playing => {
+                        run_state
+                            .set(RunState::GameOver)
+                            .unwrap();
+                    }
+                    RunState::GameOver => {
+                        run_state
+                            .set(RunState::Playing)
+                            .unwrap();
+                    }
+                }
+                
+            }
+            Interaction::Hovered => {
+                *material = button_materials.hovered.clone();
+            }
+            Interaction::None => {
+                *material = button_materials.normal.clone();
+            }
+        }
+    }
+}
+
+fn button_text_system(
+    button_query: Query<&Children, With<Button>>,
+    mut text_query: Query<&mut Text>,
+    run_state: Res<State<RunState>>,
+) {
+    let children = button_query
+        .single()
+        .expect("expected only one button");
+    let mut text = text_query
+        .get_mut(*children.first().expect(
+            "expect button to have a first child",
+        ))
+        .unwrap();
+    match run_state.current() {
+        RunState::Playing => {
+            text.sections[0].value = "End Game".to_string();
+        }
+        RunState::GameOver => {
+            text.sections[0].value = "New Game".to_string();
+        }
+    }
+}
